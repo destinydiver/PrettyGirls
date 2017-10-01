5dbdc2caecefb681c31916866178e34efb4253b1
Charles Winters <destinydiver@gmail.com>
2017-10-01 12:15:05 -0400

    Created Podcast class and added Name and Subject properties.

---
+++ PrettyGirlsTest/PrettyGirlsTest/Podcast.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrettyGirlsTest
+{
+    class Podcast
+    {
+        public string Name { get; set; }
+        public string Subject { get; set; }
+    }
+}
